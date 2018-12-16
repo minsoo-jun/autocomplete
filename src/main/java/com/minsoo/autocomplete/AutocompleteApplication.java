36134a69c8687025a27bc39e5259cd8065ecf640
minsoojun <minsoo.jun75@gmail.com>
2018-12-16 21:49:45 +0900

    Init

---
+++ src/main/java/com/minsoo/autocomplete/AutocompleteApplication.java
@@ -0,0 +1,14 @@
+package com.minsoo.autocomplete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutocompleteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutocompleteApplication.class, args);
+	}
+
+}
+
